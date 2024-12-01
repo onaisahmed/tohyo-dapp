1301c66159b91ce830dba1e76a828220bbce8c5f
onaisahmed <onaisahmed02@gmail.com>
2024-12-01 16:14:38 +0500

    Added events to log vote added, registered, state change and candidate changes

--- blockchain/contracts/tohyo.sol
+++ blockchain/contracts/tohyo.sol
@@ -45,4 +45,10 @@ contract TohyoDapp {
     uint256 public votingEndTime;
     uint256 private constant MAX_VOTING_DURATION = 2 days;
     uint private constant MIX_VOTING_DURATION = 1 days;
+
+    event VoterRegistered(address indexed voter, uint256 timestamp);
+    event CandidateAdded(uint256 indexed candidateId, string name, address candidateAddress);
+    event VoteCast(address indexed voter, uint256 candidateId, uint256 timestamp);
+    event VotingStageChanged(VotingStage newStage);
+    
 }   
